7b4f2a43ffed68cc468d8d9a0a7300453cf18434
Nick Sacco <nicksacco10@gmail.com>
2023-02-17 07:27:02 -0800

    suitcase hash

--- lessons/hashes/warm_up/suitcase_hash.rb
+++ lessons/hashes/warm_up/suitcase_hash.rb
@@ -6,3 +6,12 @@ suitcase = {
 # 1. Add a key/value pair of shirts/3 to the suitcase.
 # 2. Increase the value of shirts by 1.
 # 3. Add a key/value pair of swimsuit/true to the suitcase.
+
+suitcase[:shirts] = 3
+p suitcase
+
+suitcase[:shirts] += 1
+p suitcase
+
+suitcase[:swimsuit] = true
+p suitcase
