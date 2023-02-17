bb22ecc0e315262e8046dd8ed27d20933673a4d0
Nick Sacco <nicksacco10@gmail.com>
2023-02-17 07:22:37 -0800

    grocery list hash

--- lessons/hashes/warm_up/grocery_list_array.rb
+++ lessons/hashes/warm_up/grocery_list_array.rb
@@ -7,5 +7,10 @@ fridge_items_1 = ["milk", "eggs", "eggs","eggs", "eggs",
 
 # 1. What is problematic about listing `fridge_items_1`
 # as an array structured grocery list?
+# It shows multiples of items instead of grouping them together with a value. Each item should
+# be a key, with integers as the values indicating how many of each item there are
 
 # 2. What might be a more readable way to structure this?
+
+fridge_items_1 = {:milk => 1, :eggs => 6, :avocado => 2, :tortilla => 9}
+p fridge_items_1
