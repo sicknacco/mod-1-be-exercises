d142c3157b1794f1171d3f047b2939919fffe1a0
Nick Sacco <nicksacco10@gmail.com>
2023-02-17 07:17:37 -0800

    Symbols

--- lessons/hashes/practice/symbols.rb
+++ lessons/hashes/practice/symbols.rb
@@ -14,7 +14,13 @@ new_tv = {
 # Lets change the hash above to use symbols instead of strings.
 # Syntax symbol with hash rocket:
 # create hash here
-
+new_tv = {:screen_size => 50, :price => 300, :brand => "Samsung"}
+p new_tv
 
 # Syntax symbol without hash rocket:
 # create hash here
+new_tv = Hash.new
+new_tv[:screen_size] = 50
+new_tv[:price] = 300
+new_tv[:brand] = "Samsung"
+p new_tv
