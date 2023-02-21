097db74d89a17d3069f44b4cafdca46d5477ffe0
Nick Sacco <nicksacco10@gmail.com>
2023-02-21 07:55:57 -0800

    push

--- lessons/modules/orders/lib/grubhub_order.rb
+++ lessons/modules/orders/lib/grubhub_order.rb
@@ -1,14 +1,16 @@
 # ./grubhub_order.rb
+require './lib/online_order'
 class GrubhubOrder
-  def confirmation(thing)
-    "You got #{thing}."
-  end
+  include OnlineOrder
+  # def confirmation(thing)
+  #   "You got #{thing}."
+  # end
 
-  def delivery
-    "Your food will arrive in 45-60 minutes."
-  end
+  # def delivery
+  #   "Your food will arrive in 45-60 minutes."
+  # end
 
-  def review
-    "Please rate your order within 30 days."
-  end
+  # def review
+  #   "Please rate your order within 30 days."
+  # end
 end
--- lessons/modules/orders/lib/online_order.rb
+++ lessons/modules/orders/lib/online_order.rb
@@ -1,1 +1,15 @@
 # ./online_order.rb
+module OnlineOrder
+
+  def confirmation(thing)
+    "You got #{thing}."
+  end
+
+  def delivery(type, time)
+    "Your #{type} will arrive in #{time}."
+  end
+
+  def review
+    "Please rate your order within 30 days."
+  end
+end
