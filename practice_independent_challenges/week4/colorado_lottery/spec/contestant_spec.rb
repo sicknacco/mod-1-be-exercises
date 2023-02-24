61b3bfd327b4ff42852d1afee3873c1c09693930
Nick Sacco <nicksacco10@gmail.com>
2023-02-23 20:00:18 -0800

    Test: Initialize test

--- practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
@@ -1,3 +1,18 @@
 require 'rspec'
 require "./lib/contestant"
 require "./lib/game"
+
+RSpec.describe Contestant do
+  before(:each) do
+    @alexander = Contestant.new({first_name: 'Alexander',
+      last_name: 'Aigiades',
+      age: 28,
+      state_of_residence: 'CO',
+      spending_money: 10})
+  end
+
+  it 'exists' do
+    expect(@alexander).to be_a(Contestant)
+    expect(@alexander[:last_name]).to eq('Aigiades')
+  end
+end
