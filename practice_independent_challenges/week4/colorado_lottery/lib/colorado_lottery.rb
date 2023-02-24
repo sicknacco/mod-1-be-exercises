0502d837f7b581ae1397d19c40077025679673e4
Nick Sacco <nicksacco10@gmail.com>
2023-02-24 07:38:03 -0800

    Feat: Interested and 18

--- practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
+++ practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
@@ -2,10 +2,18 @@ class ColoradoLottery
   attr_reader :registered_contestants,
               :winners,
               :current_contestants
-              
+
   def initialize
     @registered_contestants = {}
     @winners = []
     @current_contestants = {}
+  end
+
+  def interested_and_18?(contestant, game)
+    if contestant.game_interests.include?(game.name) && (contestant.age >= 18)
+      true
+    else
+      false
+    end
   end
 end
