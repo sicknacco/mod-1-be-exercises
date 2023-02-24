58509f0b7789606698c112a5a6c5659498ec71f0
Nick Sacco <nicksacco10@gmail.com>
2023-02-23 18:42:55 -0800

    Complete iteration 2

--- practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 RSpec.describe Dock do
   before(:each) do
     @dock = Dock.new("The Rowing Dock", 3)
+    @kayak_1 = Boat.new(:kayak, 20)
+    @kayak_2 = Boat.new(:kayak, 20)
+    @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @patrick = Renter.new("Patrick Star", "4242424242424242")
+    @eugene = Renter.new("Eugene Crabs", "1313131313131313")
   end
 
   it 'exists' do
@@ -15,5 +20,15 @@ RSpec.describe Dock do
 
   it 'dock has max rental time' do
     expect(@dock.max_rental_time).to eq(3)
+  end
+
+  it 'can rent a boat and log' do
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @dock.rent(@sup_1, @eugene)
+
+    expect(@dock.rental_log).to be_a(Hash)
+    expect(@dock.rental_log.keys).to eq([@kayak_1, @kayak_2, @sup_1])
+    expect(@dock.rental_log.values).to eq([@patrick, @patrick, @eugene])
   end
 end
