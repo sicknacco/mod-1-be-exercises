317ff25d37af2021354bd571ed92d87f78d7c840
Nick Sacco <nicksacco10@gmail.com>
2023-02-23 19:50:09 -0800

    Test: name and cost tests

--- practice_independent_challenges/week4/colorado_lottery/spec/game_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/game_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Game do
     expect(@pick_4).to be_a(Game)
     expect(@mega_millions).to be_a(Game)
   end
+
+  it 'game has a name' do
+    expect(@mega_millions.name).to eq('Mega Millions')
+    expect(@pick_4.name).to eq('Pick 4')
+  end
+
+  xit 'game has a cost' do
+    expect(@mega_millions.cost).to eq(5)
+    expect(@pick_4.cost).to eq(2)
+  end
 end
