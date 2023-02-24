6037e33600fd275b06e01181c6c691d2961f2f01
Nick Sacco <nicksacco10@gmail.com>
2023-02-23 19:18:17 -0800

    Feat: Charge method

--- practice_independent_challenges/week4/boat_rental/lib/dock.rb
+++ practice_independent_challenges/week4/boat_rental/lib/dock.rb
@@ -5,11 +5,22 @@ class Dock
 
   def initialize(name, max_rental_time)
     @name = name
-    @max_rental_time = max_rental_time
+    @max_rental_time = 3
     @rental_log = {}
   end
 
   def rent(boat, renter)
     @rental_log[boat] = renter
+  end
+
+  def charge(boat)
+    max = (boat.price_per_hour * @max_rental_time)
+    charged = Hash.new(0)
+    charged[:card_number] = @rental_log[boat].credit_card_number
+    charged[:amount] = boat.price_per_hour * boat.hours_rented
+    if charged[:amount] >= max
+      charged[:amount] = max
+    end
+    charged
   end
 end
