717fd6eb5136840bf122ecd48ebf459e572b58a1
Nick Sacco <nicksacco10@gmail.com>
2023-02-23 20:19:28 -0800

    lib/contestant.rb

--- practice_independent_challenges/week4/colorado_lottery/lib/contestant.rb
+++ practice_independent_challenges/week4/colorado_lottery/lib/contestant.rb
@@ -1,7 +1,20 @@
 class Contestant
-  attr_reader :full_name
+  attr_reader :full_name,
+              :age,
+              :state_of_residence,
+              :spending_money,
+              :game_interests
 
   def initialize(contestant)
     @full_name = contestant[:first_name] + " " + contestant[:last_name]
+    @age = contestant[:age]
+    @state_of_residence = contestant[:state_of_residence]
+    @spending_money = contestant[:spending_money]
+    @game_interests = []
+  end
+
+  def out_of_state?
+    return true if state_of_residence != "CO"
+    false
   end
 end
