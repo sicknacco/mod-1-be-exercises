3e72511a8b85a585ab6f650c2109f5e4ff6a365b
Nick Sacco <nicksacco10@gmail.com>
2023-02-23 17:34:10 -0800

    Test: renter initialize tests

--- practice_independent_challenges/week4/boat_rental/spec/renter_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/renter_spec.rb
@@ -1,1 +1,19 @@
 require 'spec_helper'
+
+RSpec.describe Renter do
+  before(:each) do
+    @renter = Renter.new("Patrick Star", "4242424242424242")
+  end
+
+  it 'exists' do
+    expect(@renter).to be_a(Renter)
+  end
+
+  it 'has a name' do
+    expect(@renter.name).to eq("Patrick Star")
+  end
+
+  it 'has a credit card number' do
+    expect(@renter.credit_card_number).to eq("4242424242424242")
+  end
+end
