4f46c050419e6b61237a581e9b855e854e8dc54e
Nick Sacco <nicksacco10@gmail.com>
2023-02-23 19:41:46 -0800

    Test: Initialize test

--- practice_independent_challenges/week4/colorado_lottery/spec/game_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/game_spec.rb
@@ -1,3 +1,15 @@
 require 'rspec'
 require "./lib/contestant"
 require "./lib/game"
+
+RSpec.describe Game do
+  before(:each) do
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  it 'exists' do
+    expect(@pick_4).to be_a(Game)
+    expect(@mega_millions).to be_a(Game)
+  end
+end
