5d6ae779dc191532d86617d918f2f523ebe3221c
Nick Sacco <nicksacco10@gmail.com>
2023-02-23 19:56:33 -0800

    Feat: national_drawing method

--- practice_independent_challenges/week4/colorado_lottery/lib/game.rb
+++ practice_independent_challenges/week4/colorado_lottery/lib/game.rb
@@ -1,6 +1,15 @@
 class Game
-  attr_reader :name
+  attr_reader :name,
+              :cost,
+              :national_drawing
+
   def initialize(name, cost, national_drawing = false)
     @name = name
+    @cost = cost
+    @national_drawing = national_drawing
+  end
+
+  def national_drawing?
+    @national_drawing
   end
 end
