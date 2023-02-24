c30727bad5f398fb9d5c4f9e240e727a6c8814b4
Nick Sacco <nicksacco10@gmail.com>
2023-02-23 17:59:58 -0800

    Test: Add name and max time tests

--- practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Dock do
   it 'exists' do
     expect(@dock).to be_a(Dock)
   end
+
+  it 'dock has a name' do
+    expect(@dock.name).to eq("The Rowing Dock")
+  end
+
+  it 'dock has max rental time' do
+    expect(@dock.max_rental_time).to eq(3)
+  end
 end
