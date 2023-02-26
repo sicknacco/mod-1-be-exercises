5b9901c18813a2292d62fff59408abe3cf89eeba
Nick Sacco <nicksacco10@gmail.com>
2023-02-25 20:12:06 -0800

    Test: running cost test

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Reunion do
   before(:each) do
     @reunion = Reunion.new("1406 BE")
     @activity_1 = Activity.new("Brunch")
+    @activity_2 = Activity.new("Drinks")
   end
 
   it 'exists with a name and no activities' do
@@ -21,7 +22,7 @@ RSpec.describe Reunion do
     expect(@reunion.activities).to eq([@activity_1])
   end
 
-  it 'can calculate total cost for brunch' do
+  it 'can calculate total cost after brunch' do
     @activity_1.add_participant("Maria", 20)
     @activity_1.add_participant("Luther", 40)
 
@@ -30,5 +31,20 @@ RSpec.describe Reunion do
     expect(@activity_1).to be_a Activity
     expect(@activity_1.participants).to eq({"Maria" => 20, "Luther" => 40})
     expect(@reunion.total_cost).to eq(60)
+  end
+
+  it 'can calculate running total cost after drinks' do
+    @activity_1.add_participant("Maria", 20)
+    @activity_1.add_participant("Luther", 40)
+    @reunion.add_activity(@activity_1)
+    expect(@reunion.total_cost).to eq(60)
+
+    @activity_2.add_participant("Maria", 60)
+    @activity_2.add_participant("Luther", 60)
+    @activity_2.add_participant("Louis", 0)
+
+    @reunion.add_activity(@activity_2)
+
+    expect(@reunion.total_cost).to eq(180)
   end
 end
