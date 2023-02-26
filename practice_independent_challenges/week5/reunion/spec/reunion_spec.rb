be5933a74ef79d327db3a30ff44b8654e2f0d66e
Nick Sacco <nicksacco10@gmail.com>
2023-02-25 19:42:12 -0800

    Test: add activities test

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -4,12 +4,19 @@ require './lib/reunion'
 
 RSpec.describe Reunion do
   before(:each) do
-    @reunion = Reunion.new("1406 BE") 
+    @reunion = Reunion.new("1406 BE")
+    @activity_1 = Activity.new("Brunch")
   end
 
   it 'exists with a name and no activities' do
     expect(@reunion).to be_a Reunion
     expect(@reunion.name).to eq("1406 BE")
     expect(@reunion.activities).to eq([])
+  end
+
+  it 'can add an activitiy' do
+    @reunion.add_activity(@activity_1)
+
+    expect(@reunion.activities).to eq([@activity_1])
   end
 end
