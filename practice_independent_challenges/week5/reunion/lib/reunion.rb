04b8af8e5e8c1127b9d83290b946624e3f2ef304
Nick Sacco <nicksacco10@gmail.com>
2023-02-25 20:04:53 -0800

    Feat: total cost added to activity

--- practice_independent_challenges/week5/reunion/lib/reunion.rb
+++ practice_independent_challenges/week5/reunion/lib/reunion.rb
@@ -1,13 +1,16 @@
 class Reunion
   attr_reader :name,
-              :activities
+              :activities,
+              :total_cost
 
   def initialize(name)
     @name = name
     @activities = []
+    @total_cost = 0
   end
 
   def add_activity(activity)
     @activities << activity
+    @total_cost += activity.total_cost
   end
 end
