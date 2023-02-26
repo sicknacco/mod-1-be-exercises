c82e4a210ec43cd36c3cea4482e85a5d46bbf891
Nick Sacco <nicksacco10@gmail.com>
2023-02-25 20:04:26 -0800

    Test: total cost for brunch

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -16,8 +16,19 @@ RSpec.describe Reunion do
 
   it 'can add an activitiy' do
     @reunion.add_activity(@activity_1)
-    
+
     expect(@reunion.name).to eq("1406 BE")
     expect(@reunion.activities).to eq([@activity_1])
+  end
+
+  it 'can calculate total cost for brunch' do
+    @activity_1.add_participant("Maria", 20)
+    @activity_1.add_participant("Luther", 40)
+
+    @reunion.add_activity(@activity_1)
+
+    expect(@activity_1).to be_a Activity
+    expect(@activity_1.participants).to eq({"Maria" => 20, "Luther" => 40})
+    expect(@reunion.total_cost).to eq(60)
   end
 end
