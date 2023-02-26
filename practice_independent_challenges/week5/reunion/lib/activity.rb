9bd322f2ed7335b7b66c21f64ab2a2a9631bbd18
Nick Sacco <nicksacco10@gmail.com>
2023-02-25 18:45:23 -0800

    Feat: Working add_participant method

--- practice_independent_challenges/week5/reunion/lib/activity.rb
+++ practice_independent_challenges/week5/reunion/lib/activity.rb
@@ -1,9 +1,16 @@
 class Activity
   attr_reader :name,
-              :participants
+              :participants,
+              :total_cost
 
   def initialize(name)
     @name = name
     @participants = {}
+    @total_cost = 0
+  end
+
+  def add_participant(person, amt_paid)
+    @total_cost += amt_paid
+    @participants[person] = amt_paid
   end
 end
