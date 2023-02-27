3b5b5e514831bd818b46f5cdb4d2e9dc6b13362c
Nick Sacco <nicksacco10@gmail.com>
2023-02-26 19:38:44 -0800

    Feat: Complete iteration 1

--- practice_independent_challenges/week5/museo/lib/artist.rb
+++ practice_independent_challenges/week5/museo/lib/artist.rb
@@ -4,12 +4,16 @@ class Artist
               :born,
               :died,
               :country
-              
+
   def initialize(hash)
     @id = hash[:id]
     @name = hash[:name]
     @born = hash[:born]
     @died = hash[:died]
     @country = hash[:country]
+  end
+
+  def age_at_death
+    @died.to_i - @born.to_i
   end
 end
