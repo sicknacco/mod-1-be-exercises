f0693cfa16a64afba43056a0bd65a63671ccb054
Nick Sacco <nicksacco10@gmail.com>
2023-02-26 19:49:03 -0800

    Test: Init tests

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -1,1 +1,15 @@
 require 'spec_helper'
+
+RSpec.describe do
+  before(:each) do
+    @curator = Curator.new
+  end
+
+  it 'exists' do
+    expect(@curator).to be_a Curator
+  end
+
+  xit 'begins with empty photograph array' do
+    epect(@curator.photographs).to eq([])
+  end
+end
