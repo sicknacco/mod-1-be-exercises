0721a4dafa113fa5e7e27e05a077a5a4403d87c2
Nick Sacco <nicksacco10@gmail.com>
2023-02-26 20:57:19 -0800

    Test: photos by artist test

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -80,4 +80,68 @@ RSpec.describe do
     expect(@curator.artists).to eq([artist_1, artist_2])
     expect(@curator.find_artist_by_id("1")).to eq(artist_1)
   end
+
+  describe 'iteration 3' do
+    before(:each) do
+    @photo_1 = Photograph.new({
+      id: "1",      
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+      artist_id: "1",      
+      year: "1954"      
+    })
+    @photo_2 = Photograph.new({    
+      id: "2",      
+      name: "Moonrise, Hernandez",      
+      artist_id: "2",      
+      year: "1941"      
+    })
+    @photo_3 = Photograph.new({    
+      id: "3",      
+      name: "Identical Twins, Roselle, New Jersey",      
+      artist_id: "3",      
+      year: "1967"      
+    })
+    @photo_4 = Photograph.new({    
+      id: "4",      
+      name: "Monolith, The Face of Half Dome",      
+      artist_id: "3",      
+      year: "1927"      
+    })
+
+    @artist_1 = Artist.new({    
+      id: "1",      
+      name: "Henri Cartier-Bresson",      
+      born: "1908",      
+      died: "2004",      
+      country: "France"      
+    })
+    @artist_2 = Artist.new({    
+      id: "2",      
+      name: "Ansel Adams",      
+      born: "1902",      
+      died: "1984",      
+      country: "United States"      
+    })
+    @artist_3 = Artist.new({    
+      id: "3",      
+      name: "Diane Arbus",      
+      born: "1923",      
+      died: "1971",      
+      country: "United States"      
+    })
+    end
+
+    it 'can find photos by artist' do
+      @curator.add_artist(@artist_1)
+      @curator.add_artist(@artist_2)
+      @curator.add_artist(@artist_3)
+
+      @curator.add_photograph(@photo_1)
+      @curator.add_photograph(@photo_2)
+      @curator.add_photograph(@photo_3)
+      @curator.add_photograph(@photo_4)
+
+      expect(@curator.photographs_by_artist).to eq(Hash)
+    end
+  end
 end
