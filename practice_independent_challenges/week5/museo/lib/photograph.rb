c93229c380349a048c169e8fb810bd21d37b4151
Nick Sacco <nicksacco10@gmail.com>
2023-02-26 19:23:21 -0800

    Feat: attributes added to init

--- practice_independent_challenges/week5/museo/lib/photograph.rb
+++ practice_independent_challenges/week5/museo/lib/photograph.rb
@@ -1,4 +1,13 @@
 class Photograph
-  def initialize(attributes)
+  attr_reader :id,
+              :name,
+              :artist_id,
+              :year
+              
+  def initialize(hash)
+    @id = hash[:id]
+    @name = hash[:name]
+    @artist_id = hash[:artist_id]
+    @year = hash[:year]
   end
 end
