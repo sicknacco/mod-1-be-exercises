7b6d1d931b9528f24fd892b8a7b3533435d7fa4a
Nick Sacco <nicksacco10@gmail.com>
2023-02-26 19:22:52 -0800

    Test: attributes tests

--- practice_independent_challenges/week5/museo/spec/photo_spec.rb
+++ practice_independent_challenges/week5/museo/spec/photo_spec.rb
@@ -16,4 +16,11 @@ RSpec.describe Photograph do
   it 'exists' do
     expect(@photograph).to be_a Photograph
   end
+
+  it 'has attributes' do
+    expect(@photograph.id).to eq("1")
+    expect(@photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
+    expect(@photograph.artist_id).to eq("4")
+    expect(@photograph.year).to eq("1954")
+  end
 end
