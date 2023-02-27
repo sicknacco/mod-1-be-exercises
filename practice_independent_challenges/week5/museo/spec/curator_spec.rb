d43983a5e8295ac11d0d0f3f8fbacd3fd974cff4
Nick Sacco <nicksacco10@gmail.com>
2023-02-26 20:00:03 -0800

    Test: add_photograph method

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe do
   it 'begins with empty photograph array' do
     expect(@curator.photographs).to eq([])
   end
+
+  it 'can add photographs to array' do
+    photo_1 = Photograph.new({
+      id: "1",      
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+      artist_id: "1",      
+      year: "1954"      
+    })
+    photo_2 = Photograph.new({
+      id: "2",      
+      name: "Moonrise, Hernandez",      
+      artist_id: "2",      
+      year: "1941"      
+    })
+
+    @curator.add_photograph(photo_1)
+    @curator.add_photograph(photo_2)
+
+    expect(@curator.photographs).to eq([photo_1, photo_2])
+  end
 end
