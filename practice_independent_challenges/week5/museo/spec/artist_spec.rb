f9728cc51be1b31862af535858ee18e987d8cc58
Nick Sacco <nicksacco10@gmail.com>
2023-02-26 19:32:04 -0800

    Test: add artist attributes tests

--- practice_independent_challenges/week5/museo/spec/artist_spec.rb
+++ practice_independent_challenges/week5/museo/spec/artist_spec.rb
@@ -17,4 +17,12 @@ RSpec.describe Artist do
   it 'exists' do
     expect(@artist).to be_a Artist
   end
+
+  it 'Artist has attributes' do
+    expect(@artist.id).to eq("2")
+    expect(@artist.name).to eq("Ansel Adams")
+    expect(@artist.born).to eq("1902")
+    expect(@artist.died).to eq("1984")
+    expect(@artist.country).to eq("United States")
+  end
 end
