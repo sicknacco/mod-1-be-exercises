92c08260790771a9c1f060435c17d21ddcc5bd1a
Nick Sacco <nicksacco10@gmail.com>
2023-02-26 20:17:50 -0800

    Feat: add artist method

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -32,4 +32,28 @@ RSpec.describe do
 
     expect(@curator.photographs).to eq([photo_1, photo_2])
   end
+
+  it 'can add artists' do
+    expect(@curator.artists).to eq([])
+
+    artist_1 = Artist.new({
+      id: "1",      
+      name: "Henri Cartier-Bresson",      
+      born: "1908",      
+      died: "2004",      
+      country: "France"      
+    })
+    artist_2 = Artist.new({
+      id: "2",      
+      name: "Ansel Adams",      
+      born: "1902",      
+      died: "1984",      
+      country: "United States"      
+    })
+
+    @curator.add_artist(artist_1)
+    @curator.add_artist(artist_2)
+
+    expect(@curator.artists).to eq([artist_1, artist_2])
+  end
 end
