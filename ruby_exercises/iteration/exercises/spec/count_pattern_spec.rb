742eb7a7dbb6f988d9dee81149638727f4d5c080
Nick Sacco <nicksacco10@gmail.com>
2023-03-11 19:22:57 -0800

    wip

--- ruby_exercises/iteration/exercises/spec/count_pattern_spec.rb
+++ ruby_exercises/iteration/exercises/spec/count_pattern_spec.rb
@@ -79,19 +79,28 @@ RSpec.describe 'count pattern' do
       miguel: 50
     }
     multiple_of_three = 0
-    # Your Code Here
+    ages.each do |name, age|
+      if age % 3 == 0
+        multiple_of_three += 1
+      end
+    end
 
     expect(multiple_of_three).to eq(3)
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     family = ["alice", "bob", "charlie", "david", "eve"]
-    # Your Code Here
+    names_with_three_letters = 0
+    family.each do |name|
+      if name.length == 3
+        names_with_three_letters += 1
+      end
+    end
 
     expect(names_with_three_letters).to eq(2)
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     family = {
       mother: "alice",
       father: "bob",
@@ -99,14 +108,24 @@ RSpec.describe 'count pattern' do
       uncle: "david",
       sister: "eve"
     }
-    # Your Code Here
+    names_with_three_letters = 0
+    family.each do |relation, name|
+      if name.length == 3
+        names_with_three_letters += 1
+      end
+    end
 
     expect(names_with_three_letters).to eq(2)
   end
 
   xit 'test 9' do
     prices = [1.0, 3.9, 5.99, 18.5, 20.0]
-    # Your code goes here
+    whole_numbers = 0
+    prices.each do |price|
+      if price % price.remainder(0)
+        whole_numbers += 1
+      end
+    end
 
     expect(whole_numbers).to eq(2)
   end
